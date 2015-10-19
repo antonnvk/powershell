f7ad2e347e9c766d4497ee3f088e42c52413ef39
antonnvk <antonnvk@yandex.ru>
2015-10-19 17:24:55 +0700

    Добавил автоматическое открытие файла после завершения скрипта

--- Exchange/MailBoxStatistics.ps1
+++ Exchange/MailBoxStatistics.ps1
@@ -1,1 +1,3 @@
-﻿Get-mailbox | Get-MailboxStatistics | Sort-Object TotalItemSize -descending |ft Database, DisplayName, TotalItemSize
+﻿Get-mailbox | Get-MailboxStatistics | Sort-Object TotalItemSize -descending |ft Database, DisplayName, TotalItemSize | Out-File C:\Test.htm
+
+Invoke-Expression C:\Test.htm
